a346cb27aa947370e39c29d18c0e92462af2d59b
Randall Enfinger <randydash304@gmail.com>
2024-01-21 16:46:37 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,12 @@
+const loginPasskey = `${(secrets.LOGINPASS)}`;
+const loginUsername = `${(secrets.LOGINUSER)}`;
 
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === loginUsername && password === loginPasskey) {
+    
+  }
+});
