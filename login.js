705d94ff8c9570d2dbe9121a169840cc32aa2ba0
Randall Enfinger <randydash304@gmail.com>
2024-01-22 06:42:58 -0600

    Update login.js

--- login.js
+++ login.js
@@ -2,11 +2,19 @@ const loginPasskey = `${(secrets.LOGINPASS)}`;
 const loginUsername = `${(secrets.LOGINUSER)}`;
 
 document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
+  event.preventDefault(); // prevent the form from submitting
+
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
   if (username === loginUsername && password === loginPasskey) {
-    
+    // if the entered username and password are correct, redirect to panel.html
+    window.location.href = './pages/panel.html';
+  } else if (username !== loginUsername && password === loginPasskey) {
+    // if the entered username is incorrect, display an error message for the username
+    console.log('Username or Password is incorrect');
+  } else if (username === loginUsername && password !== loginPasskey) {
+    // if the entered password is incorrect, display an error message for the password
+    console.log('Username or Password is incorrect');
   }
 });
