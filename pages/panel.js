f5bd9a14aedd619097796a850563514c65565b35
Randall Enfinger <randydash304@gmail.com>
2024-01-22 08:57:11 -0600

    Update panel.js

--- pages/panel.js
+++ pages/panel.js
@@ -1,1 +1,22 @@
+const toggleSwitch = document.querySelector('#dark-mode-toggle');
 
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  } 
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) { 
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+ if (currentTheme === 'dark') { 
+   toggleSwitch.checked = true; 
+ }
+}
