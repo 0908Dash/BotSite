40e89abde0e92be88aad9eec685693cfcd3cc8a0
Randall Enfinger <randydash304@gmail.com>
2024-01-23 09:12:17 -0600

    Update login.js

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ document.querySelector('form').addEventListener('submit', (event) => {
 
   if (username === loginUsername && password === loginPasskey) {
     // if the entered username and password are correct, redirect to panel.html
-    window.location.href = './pages/panel.html';
+    window.location.replace = './pages/panel.html';
   } else {
     // if the entered username or password is incorrect, display an error message
     console.log('Username/Password is incorrect');
