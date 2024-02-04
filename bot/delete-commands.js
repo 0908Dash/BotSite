6a1dc7f955023dbba57b340476ce617aab088970
Randall Enfinger <randydash304@gmail.com>
2024-02-04 11:21:21 -0600

    Delete bot directory

--- bot/delete-commands.js
+++
@@ -1,8 +0,0 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./index.js');
-
-const rest = new REST().setToken(token);
-
-rest.put(Routes.applicationCommands(clientId), { body: [] })
-.then(() => console.log('Successfully deleted all application commands.'))
-.catch(console.error);
